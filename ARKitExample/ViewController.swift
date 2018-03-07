2c4f4a799d9efedbd13750417bea6dfe760bc392
maxiklinke <maxi.klinke@googlemail.com>
2018-03-07 16:53:05 +0100

    Not necessary fiels and comments removed.

--- ARKitExample/ViewController.swift
+++ ARKitExample/ViewController.swift
@@ -14,7 +14,7 @@ import Photos
 
 class ViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererDelegate {
 	
-    
+    //---------------------------------- THIS STUFF IS FOR SETTING UP THE SCENE ---------------------
     
     // MARK: - Main Setup & View Controller methods
     override func viewDidLoad() {
@@ -107,162 +107,11 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererDeleg
 		}
 	}
 	
-    /*
-    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        DispatchQueue.main.async {
-            if let planeAnchor = anchor as? ARPlaneAnchor {
-                
-            }
-        }
-    }
-	
-    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        DispatchQueue.main.async {
-            if let planeAnchor = anchor as? ARPlaneAnchor {
-                
-            }
-        }
-    }
-	
-    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-        DispatchQueue.main.async {
-            if let planeAnchor = anchor as? ARPlaneAnchor {
-                
-            }
-        }
-    }*/
-	
-
-	
-	/*
-    func session(_ session: ARSession, didFailWithError error: Error) {
-
-        guard let arError = error as? ARError else { return }
-
-        let nsError = error as NSError
-		var sessionErrorMsg = "\(nsError.localizedDescription) \(nsError.localizedFailureReason ?? "")"
-		if let recoveryOptions = nsError.localizedRecoveryOptions {
-			for option in recoveryOptions {
-				sessionErrorMsg.append("\(option).")
-			}
-		}
-
-        let isRecoverable = (arError.code == .worldTrackingFailed)
-		if isRecoverable {
-			sessionErrorMsg += "\nYou can try resetting the session or quit the application."
-		} else {
-			sessionErrorMsg += "\nThis is an unrecoverable error that requires to quit the application."
-		}
-		
-        //TODO: Error message
-	}*/
-	/*
-		
-	func sessionInterruptionEnded(_ session: ARSession) {
-		session.run(sessionConfig, options: [.resetTracking, .removeExistingAnchors])
-		restartExperience(self)
-	}*/
-	
-    // MARK: - Ambient Light Estimation
-	/*
-	func toggleAmbientLightEstimation(_ enabled: Bool) {
-		
-        if enabled {
-			if !sessionConfig.isLightEstimationEnabled {
-				// turn on light estimation
-				sessionConfig.isLightEstimationEnabled = true
-				session.run(sessionConfig)
-			}
-        } else {
-			if sessionConfig.isLightEstimationEnabled {
-				// turn off light estimation
-				sessionConfig.isLightEstimationEnabled = false
-				session.run(sessionConfig)
-			}
-        }
-    }*/
-
-    
-	
-	
-	
-	/*
-	func worldPositionFromScreenPosition(_ position: CGPoint,
-	                                     objectPos: SCNVector3?,
-	                                     infinitePlane: Bool = false) -> (position: SCNVector3?, planeAnchor: ARPlaneAnchor?, hitAPlane: Bool) {
-		
-		// -------------------------------------------------------------------------------
-		// 1. Always do a hit test against exisiting plane anchors first.
-		//    (If any such anchors exist & only within their extents.)
-		
-		let planeHitTestResults = sceneView.hitTest(position, types: .existingPlaneUsingExtent)
-		if let result = planeHitTestResults.first {
-			
-			let planeHitTestPosition = SCNVector3.positionFromTransform(result.worldTransform)
-			let planeAnchor = result.anchor
-			
-			// Return immediately - this is the best possible outcome.
-			return (planeHitTestPosition, planeAnchor as? ARPlaneAnchor, true)
-		}
-		
-		// -------------------------------------------------------------------------------
-		// 2. Collect more information about the environment by hit testing against
-		//    the feature point cloud, but do not return the result yet.
-		
-		var featureHitTestPosition: SCNVector3?
-		var highQualityFeatureHitTestResult = false
-		
-		let highQualityfeatureHitTestResults = sceneView.hitTestWithFeatures(position, coneOpeningAngleInDegrees: 18, minDistance: 0.2, maxDistance: 2.0)
-		
-		if !highQualityfeatureHitTestResults.isEmpty {
-			let result = highQualityfeatureHitTestResults[0]
-			featureHitTestPosition = result.position
-			highQualityFeatureHitTestResult = true
-		}
-		
-		// -------------------------------------------------------------------------------
-		// 3. If desired or necessary (no good feature hit test result): Hit test
-		//    against an infinite, horizontal plane (ignoring the real world).
-		
-		if (infinitePlane && dragOnInfinitePlanesEnabled) || !highQualityFeatureHitTestResult {
-			
-			let pointOnPlane = objectPos ?? SCNVector3Zero
-			
-			let pointOnInfinitePlane = sceneView.hitTestWithInfiniteHorizontalPlane(position, pointOnPlane)
-			if pointOnInfinitePlane != nil {
-				return (pointOnInfinitePlane, nil, true)
-			}
-		}
-		
-		// -------------------------------------------------------------------------------
-		// 4. If available, return the result of the hit test against high quality
-		//    features if the hit tests against infinite planes were skipped or no
-		//    infinite plane was hit.
-		
-		if highQualityFeatureHitTestResult {
-			return (featureHitTestPosition, nil, false)
-		}
-		
-		// -------------------------------------------------------------------------------
-		// 5. As a last resort, perform a second, unfiltered hit test against features.
-		//    If there are no features in the scene, the result returned here will be nil.
-		
-		let unfilteredFeatureHitTestResults = sceneView.hitTestWithFeatures(position)
-		if !unfilteredFeatureHitTestResults.isEmpty {
-			let result = unfilteredFeatureHitTestResults[0]
-			return (result.position, nil, false)
-		}
-		
-		return (nil, nil, false)
-	}
-	
-	// Use average of recent virtual object distances to avoid rapid changes in object scale.
-	var recentVirtualObjectDistances = [CGFloat]()
-	*/
+ 
 
 
    
-	
+	//---------------------------------- THIS STUFF IS FOR USER INTERACTION -------------------------------
 	
 	@IBOutlet weak var restartExperienceButton: UIButton!
 	var restartExperienceButtonIsEnabled = true
@@ -328,16 +177,40 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererDeleg
     
 		
 
+    //---------------------------------- MY BUTTONS ----------------------------
+    
+    
+    @IBAction func onButtonTouch(_ button: UIButton) {
+        
+        
+        run_on_background_thread {
+            
+            for _ in 1...10 {
+                self.place2DImage(width: CGFloat.random(min: 0.05, max: 0.2), distanceToUser: Float.random(min: 0.2, max: 1.0), offsetVert: Float.random(min: 0.0, max: 0.5), offsetAngle: Float.random(min: 0.0, max: 2*Float.pi), image: #imageLiteral(resourceName: "heart"))
+                self.place2DImage(width: CGFloat.random(min: 0.05, max: 0.2), distanceToUser: Float.random(min: 0.5, max: 2.0), offsetVert: Float.random(min: 0.0, max: 0.5), offsetAngle: Float.random(min: 0.0, max: 2*Float.pi), image: #imageLiteral(resourceName: "heart"), animationStartingOpacity: CGFloat.random(min: 0.7, max: 0.9), animationEndingOpacity: CGFloat.random(min: 0.4, max: 0.6), animationTranslationVector: SCNVector3Make(Float.random(min: 0.0, max: 0.1), Float.random(min: 0.0, max: 0.1), Float.random(min: 0.0, max: 0.1)), animationTime: Double.random(min: 2.0, max: 5.0), animateBack: true, animationRepeat: true)
+                
+                
+                if let url = URL(string: "https://media.giphy.com/media/3CSElA3OxWFcuk44mI/giphy.gif"){
+                    self.place2DGif(width: CGFloat.random(min: 0.05, max: 0.2), distanceToUser: Float.random(min: 0.2, max: 1.0), offsetVert: Float.random(min: 0.0, max: 0.5), offsetAngle: Float.random(min: 0.0, max: 2*Float.pi), url: url)
+                    
+                }
+                
+            }
+            
+        }
+        
+        
+    }
+    
+    func run_on_background_thread(code: @escaping () -> Void){
+        DispatchQueue.global(qos: .default).async(execute: code)
+    }
     
     
     
-    
-    // ---------------------------------------- XXXXXXXXXXXXXXXXXXXXXXXXX -----------------------------------------
-    // ------------------------------- HERE IS THE MERGE WITH THE OTHER PROJECT -----------------------------------------
-    // ---------------------------------------- XXXXXXXXXXXXXXXXXXXXXXXXX -----------------------------------------
     
 
-    //---------------------------------------------- STUFF TO PLACE OBJECTS ----------------------------------------------
+    //--------------------------------------- STUFF TO PLACE AND REMOVE OBJECTS -------------------------------------------
 
     
     
@@ -534,38 +407,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNSceneRendererDeleg
         animation.calculationMode = kCAAnimationDiscrete
         
         return animation;
-    }
-    
-    
-  
-
-    //---------------------------------- MY BUTTONS ----------------------------
-
-    
-    @IBAction func onButtonTouch(_ button: UIButton) {
-        
-        
-        run_on_background_thread {
-
-            for _ in 1...10 {
-                self.place2DImage(width: CGFloat.random(min: 0.05, max: 0.2), distanceToUser: Float.random(min: 0.2, max: 1.0), offsetVert: Float.random(min: 0.0, max: 0.5), offsetAngle: Float.random(min: 0.0, max: 2*Float.pi), image: #imageLiteral(resourceName: "heart"))
-                self.place2DImage(width: CGFloat.random(min: 0.05, max: 0.2), distanceToUser: Float.random(min: 0.5, max: 2.0), offsetVert: Float.random(min: 0.0, max: 0.5), offsetAngle: Float.random(min: 0.0, max: 2*Float.pi), image: #imageLiteral(resourceName: "heart"), animationStartingOpacity: CGFloat.random(min: 0.7, max: 0.9), animationEndingOpacity: CGFloat.random(min: 0.4, max: 0.6), animationTranslationVector: SCNVector3Make(Float.random(min: 0.0, max: 0.1), Float.random(min: 0.0, max: 0.1), Float.random(min: 0.0, max: 0.1)), animationTime: Double.random(min: 2.0, max: 5.0), animateBack: true, animationRepeat: true)
-                
-                
-                if let url = URL(string: "https://media.giphy.com/media/3CSElA3OxWFcuk44mI/giphy.gif"){
-                    self.place2DGif(width: CGFloat.random(min: 0.05, max: 0.2), distanceToUser: Float.random(min: 0.2, max: 1.0), offsetVert: Float.random(min: 0.0, max: 0.5), offsetAngle: Float.random(min: 0.0, max: 2*Float.pi), url: url)
-                    
-                }
-                
-            }
-        
-        }
-            
-        
-    }
-    
-    func run_on_background_thread(code: @escaping () -> Void){
-        DispatchQueue.global(qos: .default).async(execute: code)
     }
 
     
